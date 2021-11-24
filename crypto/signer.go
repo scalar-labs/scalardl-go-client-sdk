01cf52c684d6c92ca2233886a8c7632508f0ae22
Plenty Su <plenty.su@gmail.com>
2021-11-24 10:38:39 +0900

    Better naming

--- crypto/signer.go
+++ crypto/signer.go
@@ -2,5 +2,5 @@ package crypto
 
 // Signer defines an interface to sign messages.
 type Signer interface {
-	Sign(message []byte) (signed []byte, err error)
+	Sign(message []byte) (signature []byte, err error)
 }
--- crypto/verifier.go
+++ crypto/verifier.go
@@ -2,5 +2,5 @@ package crypto
 
 // Verifier defines an interface to verify if the message and the signature are matched.
 type Verifier interface {
-	Verify(message []byte, signature []byte) (validated bool)
+	Verify(message []byte, signature []byte) (verified bool)
 }
