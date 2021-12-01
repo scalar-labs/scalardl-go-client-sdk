239419182985a5ff121cd3b3d27c6305f838e4fd
Plenty Su <plenty.su@gmail.com>
2021-12-01 16:08:44 +0900

    Implement the client/error package (#8)

---
+++ client/error/client_error.go
@@ -0,0 +1,28 @@
+package error
+
+import "github.com/scalar-labs/dl/ledger/statuscode"
+
+// ClientError is used when ClientService has errors.
+// It implements the Error interface.
+type ClientError struct {
+	message    string
+	statusCode statuscode.StatusCode
+}
+
+// NewClientError creates the client error instance.
+func NewClientError(statusCode statuscode.StatusCode, message string) ClientError {
+	return ClientError{
+		message:    message,
+		statusCode: statusCode,
+	}
+}
+
+// Error just returns the error message.
+func (e ClientError) Error() string {
+	return e.message
+}
+
+// StatusCode returns the status code that represents the type of errors.
+func (e ClientError) StatusCode() statuscode.StatusCode {
+	return e.statusCode
+}
