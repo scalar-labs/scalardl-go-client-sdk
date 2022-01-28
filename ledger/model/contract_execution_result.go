624b64d9c45e99dfae3cbd55b94068c56765b1f9
Plenty Su <plenty.su@gmail.com>
2022-01-28 10:28:25 +0900

    Implement the ledger/model package (#14)

---
+++ ledger/model/contract_execution_result.go
@@ -0,0 +1,74 @@
+package model
+
+import (
+	"fmt"
+
+	"github.com/scalar-labs/dl"
+	"github.com/scalar-labs/dl/ledger/asset"
+)
+
+// ContractExecutionResult defines the result of a contract execution.
+// It contains the result of the contract execution along with a list of asset proofs from Ledger and Auditor.
+type ContractExecutionResult struct {
+	Result        dl.JSONObject
+	Proofs        []asset.Proof
+	AuditorProofs []asset.Proof
+}
+
+// Equal checks if two contract execution results have the same values.
+func (r ContractExecutionResult) Equal(another ContractExecutionResult) (equal bool) {
+	var (
+		myResult      dl.JSONObject = r.Result
+		anotherResult dl.JSONObject = another.Result
+	)
+
+	if (myResult == nil && anotherResult != nil) || (myResult != nil && anotherResult == nil) {
+		return false
+	}
+
+	if !myResult.Equal(anotherResult) {
+		return false
+	}
+
+	if len(r.Proofs) != len(another.Proofs) {
+		return false
+	}
+
+	var anotherProofsInMap map[string]asset.Proof = make(map[string]asset.Proof)
+
+	for _, p := range another.Proofs {
+		key := fmt.Sprintf("%s-%d", p.ID, p.Age)
+		anotherProofsInMap[key] = p
+	}
+
+	for _, p1 := range r.Proofs {
+		key := fmt.Sprintf("%s-%d", p1.ID, p1.Age)
+		p2, found := anotherProofsInMap[key]
+
+		if !found || !p1.Equal(p2) {
+			return false
+		}
+	}
+
+	if len(r.AuditorProofs) != len(another.AuditorProofs) {
+		return false
+	}
+
+	var anotherAuditorProofsInMap map[string]asset.Proof = make(map[string]asset.Proof)
+
+	for _, p := range another.AuditorProofs {
+		key := fmt.Sprintf("%s-%d", p.ID, p.Age)
+		anotherAuditorProofsInMap[key] = p
+	}
+
+	for _, p1 := range r.AuditorProofs {
+		key := fmt.Sprintf("%s-%d", p1.ID, p1.Age)
+		p2, found := anotherAuditorProofsInMap[key]
+
+		if !found || !p1.Equal(p2) {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ ledger/model/model_test.go
@@ -0,0 +1,213 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/scalar-labs/dl"
+	"github.com/scalar-labs/dl/ledger/asset"
+	"github.com/scalar-labs/dl/ledger/statuscode"
+)
+
+func TestLedgerValidationResult_Equal(t *testing.T) {
+	shouldBeTrue := LedgerValidationResult{
+		Code: statuscode.OK,
+		Proof: asset.Proof{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0x00, 0x11},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		},
+		AuditorProof: asset.Proof{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0xCC, 0xDD},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		},
+	}.Equal(LedgerValidationResult{
+		Code: statuscode.OK,
+		Proof: asset.Proof{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0x00, 0x11},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		},
+		AuditorProof: asset.Proof{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0xCC, 0xDD},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		},
+	})
+
+	if !shouldBeTrue {
+		t.Errorf("two identitical LedgerValidationResult should be equal")
+	}
+
+	shouldBeFalse := LedgerValidationResult{
+		Code: statuscode.OK,
+		Proof: asset.Proof{
+			ID:        "bar",
+			Age:       1,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0x00, 0x11},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		},
+		AuditorProof: asset.Proof{
+			ID:        "bar",
+			Age:       1,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0xCC, 0xDD},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		},
+	}.Equal(LedgerValidationResult{
+		Code: statuscode.OK,
+		Proof: asset.Proof{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0x00, 0x11},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		},
+		AuditorProof: asset.Proof{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0xCC, 0xDD},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		},
+	})
+
+	if shouldBeFalse {
+		t.Errorf("two differenct LedgerValidationResult should not be equal")
+	}
+}
+
+func TestContractExecutionResult_Equal(t *testing.T) {
+	shouldBeTrue := ContractExecutionResult{
+		Result: dl.JSONObject{"argument": "parameter"},
+		Proofs: []asset.Proof{{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0x00, 0x11},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		}},
+		AuditorProofs: []asset.Proof{{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0xCC, 0xDD},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		}},
+	}.Equal(ContractExecutionResult{
+		Result: dl.JSONObject{"argument": "parameter"},
+		Proofs: []asset.Proof{{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0x00, 0x11},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		}},
+		AuditorProofs: []asset.Proof{{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0xCC, 0xDD},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		}},
+	})
+
+	if !shouldBeTrue {
+		t.Errorf("two identitical ContractExecutionResult should be equal")
+	}
+
+	shouldBeFalse := ContractExecutionResult{
+		Result: dl.JSONObject{"argument": "parameter"},
+		Proofs: []asset.Proof{{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0x00, 0x11},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		}},
+		AuditorProofs: []asset.Proof{{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0xCC, 0xDD},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		}},
+	}.Equal(ContractExecutionResult{
+		Result: dl.JSONObject{"argument": "parameter"},
+		Proofs: []asset.Proof{{
+			ID:        "bar",
+			Age:       0,
+			Input:     dl.JSONObject{},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0x00, 0x11},
+			Key:       asset.ProofKey{ID: "bar", Age: 0},
+		}},
+		AuditorProofs: []asset.Proof{{
+			ID:        "foo",
+			Age:       999,
+			Input:     dl.JSONObject{"argument": "parameter"},
+			Nonce:     "i-am-nonce",
+			Hash:      []byte{0x00, 0x01},
+			PrevHash:  []byte{0x11, 0x22},
+			Signature: []byte{0xCC, 0xDD},
+			Key:       asset.ProofKey{ID: "foo", Age: 999},
+		}},
+	})
+
+	if shouldBeFalse {
+		t.Errorf("two differenct ContractExecutionResult should not be equal")
+	}
+}
